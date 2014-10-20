015ec3b654f857ed99e958b20a92ce80bb346ef7
Shlomi Matichin <shlomi@stratoscale.com>
2014-10-20 17:56:33 +0000

    Bugfix: filesystemabstraction uses connection from pool even if hours passed. Limited to 60 seconds

--- logbeam/ftpfilesystemabstraction.py
+++ logbeam/ftpfilesystemabstraction.py
@@ -2,11 +2,18 @@ from logbeam import config
 from logbeam import ftputilcustomport
 import threading
 import contextlib
+import time
+import collections
+
+
+_Connection = collections.namedtuple("_Connection", ["connection", "lastTouch"])
 
 
 class FTPFilesystemAbstraction:
+    _STALE_TIMEOUT = 60
+
     def __init__(self):
-        self._connectionPool = [self._createConnection()]
+        self._connectionPool = [_Connection(self._createConnection(), time.time())]
         self._connectionPoolLock = threading.Lock()
 
     def _createConnection(self):
@@ -23,14 +30,17 @@ class FTPFilesystemAbstraction:
 
     def close(self):
         while len(self._connectionPool) > 0:
-            self._connectionPool.pop().close()
+            self._connectionPool.pop().connection.close()
 
     @contextlib.contextmanager
     def filesystem(self):
         connection = None
+        timeout = time.time() - self._STALE_TIMEOUT
         with self._connectionPoolLock:
+            while len(self._connectionPool) > 0 and self._connectionPool[0].lastTouch < timeout:
+                self._connectionPool.pop(0).connection.close()
             if len(self._connectionPool) > 0:
-                connection = self._connectionPool.pop()
+                connection = self._connectionPool.pop().connection
         if connection is None:
             connection = self._createConnection()
         yield connection
@@ -38,4 +48,4 @@ class FTPFilesystemAbstraction:
             if len(self._connectionPool) > 5:
                 connection.close()
             else:
-                self._connectionPool.append(connection)
+                self._connectionPool.append(_Connection(connection, time.time()))
